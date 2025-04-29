9891d1adb1f3a348893bb7f5673417a3654f6f78
sabbir292 <113119826+sabbir292@users.noreply.github.com>
2025-04-29 09:50:08 +0530

    added Herosection, Button, Components, alias

---
+++ svelte/developer_portfolio/src/lib/components/sections/HeroSection.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+</script>
+
+<section class="hero-section default-margin">
+    <h3>Hello! I'm Sabbir</h3>
+    <div class="underscore mb-m"></div>
+    <h1>I'm a Frontend developer crafting intuitive and performant</h1>
+    <h1 class="dark-grey">web experiences.</h1>
+    <button {onclick}>Let's talk</button>
+</section>
+
+<style>
+    h3{
+        padding-top: 4rem;
+    }
+</style>
